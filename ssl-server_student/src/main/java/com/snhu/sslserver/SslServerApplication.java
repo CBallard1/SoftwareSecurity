bd80c9b61ac237aa130252466921ebd3806b86d5
tayofleetfoot <80224904+tayofleetfoot@users.noreply.github.com>
2021-10-11 20:37:27 -0500

    Create REST Controller for Check Sum

--- ssl-server_student/src/main/java/com/snhu/sslserver/SslServerApplication.java
+++ ssl-server_student/src/main/java/com/snhu/sslserver/SslServerApplication.java
@@ -2,6 +2,12 @@ package com.snhu.sslserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import java.security.*;
+import javax.xml.bind.DatatypeConverter;
+
 
 @SpringBootApplication
 public class SslServerApplication {
@@ -12,3 +18,25 @@ public class SslServerApplication {
 
 }
 //FIXME: Add route to enable check sum return of static data example:  String data = "Hello World Check Sum!";
+
+@RestController
+class ServerController{
+	@RequestMapping("/hash")
+	public String dataCheckSum(@RequestParam(value="data", defaultValue = "Hello World Check Sum!") String input) throws NoSuchAlgorithmException{
+		String algorithm = "SHA-256";
+		try {
+			MessageDigest md = MessageDigest.getInstance(algorithm);
+			byte[] hashedValue = md.digest(input.getBytes());
+			return bytesToHex(hashedValue);
+		} catch (NoSuchAlgorithmException e) {
+			//TODO: handle exception
+			e.printStackTrace();
+		}
+
+		return "Empty";
+	}
+
+	private static String bytesToHex(byte[] hash){
+		return DatatypeConverter.printHexBinary(hash).toLowerCase();
+	}
+}
